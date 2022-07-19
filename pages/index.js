86ca23d86e3b097bc54781f52c9c6ed717deaf16
winthemillion <winthemillion@gmail.com>
2022-07-19 14:30:31 -0400

    12.1

--- pages/index.js
+++ pages/index.js
@@ -163,7 +163,7 @@ window.addEventListener('DOMContentLoaded', event => {
                     // date={Date.now() + 259200000}
                     //  date={1655959589000}
                     // TERMINA EL JUEVES A LAS 11:59:59
-                    date={1657929600000}
+                    date={1658664000000}
                     renderer={renderer}
                   />
 
